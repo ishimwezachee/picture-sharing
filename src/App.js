038dc81b88c2550eaff70a939c633302702907f6
zachee <zacheeishimwe@gmail.com>
2020-04-24 06:08:24 +0200

    rendering user places

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import {
 import Users from "./user/pages/User";
 import NewPlaces from "./places/pages/NewPlaces";
 import MainNavigation from "./shared/components/Navigation/MainNavigation";
+import UserPlaces from "./places/pages/UserPlace"
 const App = () =>{
   return (
     <Router>
@@ -17,7 +18,9 @@ const App = () =>{
 <Route path="/" exact >
    <Users/>
   </Route>
-
+<Route path="/:userId/places" exact>
+<UserPlaces />
+</Route>
   <Route path="/places/new" exact >
    <NewPlaces />
   </Route>
--- src/places/pages/UserPlace.js
+++ src/places/pages/UserPlace.js
@@ -1,7 +1,34 @@
 import React from 'react';
-
+import PlaceList from "../components/PlaceList";
+const DUMMY_PLACES =[
+    {
+        id:"p1",
+        title:"Empire State Building",
+        description:"One of the most famous sky scrppers in the world ",
+        imageUrl:"https://www.kigaliwalktour.com/wp-content/uploads/2018/10/Kigali-Convetion-Centre.jpg",
+        address:"KG 2 Roundabout, Kigali, Rwanda",
+        location:{
+            lat:-1.9545556,
+            lng:30.0916647
+        },
+        creator:"u1"
+    },
+    {
+        id:"p2",
+        title:"Empire State Building",
+        description:"One of the most famous sky scrppers in the world ",
+        imageUrl:"https://lh3.googleusercontent.com/proxy/zdEBFC7FmXFsbxJe_BnhVhI3jDUL7v6lw7n0RbAIt_byuZ60yRE_MiiPb_zzVD51DkzO0-2bwAVsyLUZGh9Wg_KSAdFihu2zSXCuCFyApGVccgUPOrrKfA",
+        address:"KG 2 Roundabout, Kigali, Rwanda",
+        location:{
+            lat:-1.9545556,
+            lng:30.0916647
+        },
+        creator:"u2"
+    }
+]
 const UserPlaces = () =>{
-    return
+
+    return <PlaceList items ={DUMMY_PLACES}/>;
 };
 
-export default UserPlaces 
+export default UserPlaces  
