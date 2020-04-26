f11da3cedbd62bd7a4acb9f48b49348f72e9ddb8
zachee <zacheeishimwe@gmail.com>
2020-04-26 06:13:10 +0200

    add share button

--- src/places/components/PlaceList.js
+++ src/places/components/PlaceList.js
@@ -1,6 +1,7 @@
 import React from "react";
  import "./PlaceList.css"
 import Card from "../../shared/components/UIElements/Card";
+import Button from "../../shared/components/FormElements/button";
 import PlaceItem from "./PlaceItem";
 const PlaceList = props =>{
 
@@ -9,7 +10,7 @@ return (
     <div className="place-list center">
     <Card>
         <h2>No places found. Maybe create one ?</h2>
-        <button>Share place</button>
+        <Button to="/places/new">Share place</Button>
     </Card>
     </div>
 )
